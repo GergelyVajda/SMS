dd58e44faa6cbd55a04bd26eb5b9b649f86c69c5
gergely.vajda <gergely.vajda@SZT-W7-10.gimilabor.waldorf-obuda.lan>
2019-09-23 14:20:34 +0200

    elso feladat

--- src/sms/SMSagy.java
+++ src/sms/SMSagy.java
@@ -10,6 +10,47 @@ package sms;
  * @author gergely.vajda
  */
 public class SMSagy {
-    
-    
+    private Integer ora;
+    private Integer perc;
+    private Integer telefonszam;
+    private String uzenet;
+
+    public void setOra(Integer ora) {
+        this.ora = ora;
+    }
+
+    public void setPerc(Integer perc) {
+        this.perc = perc;
+    }
+
+    public void setTelefonszam(Integer telefonszam) {
+        this.telefonszam = telefonszam;
+    }
+
+    public void setUzenet(String uzenet) {
+        this.uzenet = uzenet;
+    }
+
+    public Integer getOra() {
+        return ora;
+    }
+
+    public Integer getPerc() {
+        return perc;
+    }
+
+    public Integer getTelefonszam() {
+        return telefonszam;
+    }
+
+    public String getUzenet() {
+        return uzenet;
+    }
+
+    public SMSagy(Integer ora, Integer perc, Integer telefonszam, String uzenet) {
+        this.ora = ora;
+        this.perc = perc;
+        this.telefonszam = telefonszam;
+        this.uzenet = uzenet;
+    }
 }
--- src/sms/SMSeljaras.java
+++ src/sms/SMSeljaras.java
@@ -5,11 +5,51 @@
  */
 package sms;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author gergely.vajda
  */
 public class SMSeljaras {
+
     
+    private List<SMSagy> memoria;
+    
+    public SMSeljaras() {
+        this.memoria = new ArrayList<>();
+    }
+    
+    public List<SMSagy> getMemoria() {
+        return memoria;
+    }
+    private void megnyito() throws IOException{
+        Integer hosszusag=0;
+        String egysor;
+        String ketsor;
+        String[] ideiglenes=new String[3];
+        try {
+            FileReader buta=new FileReader("sms.txt");
+            BufferedReader okos= new BufferedReader(buta);
+            hosszusag=Integer.parseInt(okos.readLine());
+            for (int i = 0; i < hosszusag; i++) {
+                egysor=okos.readLine();
+                ketsor=okos.readLine();
+                ideiglenes=egysor.split(" ");
+                SMSagy agy=new SMSagy(i, i, hosszusag, ketsor);
+                
+            }
+            buta.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("A file nem található!");
+        }
+    }
     
 }
